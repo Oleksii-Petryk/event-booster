5f69d908902cba53cba381c3188835d81cdd53cc
Oleksandr Okhrimovich <ohrimovich.sanek@gmail.com>
2021-09-11 20:29:29 +0300

    [API-Service] Add logic for get events on first page load

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 import './sass/main.scss';
+import DiscoveryApiService from './js/api-service';
+
+const discoveryApiService = new DiscoveryApiService();
+
+async function getEventsOnFirstLoad() {
+    try {
+        const events = await discoveryApiService.featchEvents();
+        console.log(events);
+    } catch (error) {
+        console.log(error);
+    }
+    
+}
+
+getEventsOnFirstLoad();

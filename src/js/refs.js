f19ae0540cba367521ae7b2d49a8f341c1d8beed
Oleksandr Okhrimovich <ohrimovich.sanek@gmail.com>
2021-09-14 20:44:50 +0300

    [searchEventBYName] add logic

--- src/js/refs.js
+++ src/js/refs.js
@@ -1,4 +1,3 @@
 export default {
     mainContent: document.querySelector('.main-content'),
-    input: document.querySelector('.search-form__input-js'),
-};
+    input: document.querySelector('.search-form__input-js')};

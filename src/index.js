1582c4f133d058fdda192e208d3ad24623a485a6
Mark <yakushyn.mark@gmail.com>
2021-09-15 13:46:11 +0300

    init country_choice_logic

--- src/index.js
+++ src/index.js
@@ -5,20 +5,19 @@ import refs from './js/refs';
 import debounce from 'lodash.debounce';
 import searchByEventName from './js/searchEventsByName';
 import './js/up';
+import './js/dropdown-menu.js';
 
-refs.input.addEventListener('input', debounce(searchByEventName, 500) );
+refs.input.addEventListener('input', debounce(searchByEventName, 500));
 
 const discoveryApiService = new DiscoveryApiService();
 
 async function getEventsOnFirstLoad() {
-    try {
-        const events = await discoveryApiService.getEventsByInputValue();
-        renderEventsList(events);
-    } catch (error) {
-        console.log(error);
-    }
-    
+  try {
+    const events = await discoveryApiService.getEventsByInputValue();
+    renderEventsList(events);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 getEventsOnFirstLoad();
-

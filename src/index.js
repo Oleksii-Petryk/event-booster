2bb282adab5729913d13d71ad7abba634edf04c2
unknown <milausik@ukr.net>
2021-09-17 20:58:51 +0300

    delete up

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { renderEventsList, clearEventsList } from './js/render-events.js';
 import refs from './js/refs';
 import debounce from 'lodash.debounce';
 import searchByEventName from './js/searchEventsByName';
-import './js/up';
 import './js/dropdown-menu.js';
 import './js/country-selection-logic';
 import './js/searchEventByCountryName';
--- src/js/refs.js
+++ src/js/refs.js
@@ -2,11 +2,10 @@ export default {
 
   mainContent: document.querySelector('.main-content'),
   input: document.querySelector('.search-form__input-js'),
-  toUp: document.querySelector('.back_to_top'),
   selected: document.querySelector('.selected'),
   optionsContainer: document.querySelector('.options__container'),
   optionsList: document.querySelectorAll('.option'),
   body: document.querySelector('.body__bg'),
-      mask: document.querySelector('.mask'),
+  mask: document.querySelector('.mask'),
 
 };

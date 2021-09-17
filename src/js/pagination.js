19be2e6a59b1710ce37ee545bbf888031b0e18b8
Kapral-Yrii <kapral2701@gmail.com>
2021-09-17 19:17:38 +0300

    fixed style buttons pagination and logic search country code

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -1,5 +1,4 @@
 import Pagination from 'tui-pagination';
-import 'tui-pagination/dist/tui-pagination.css';
 import DiscoveryApiService from './api-service';
 import refs from './refs.js';
 import countryDatabase from './countryDatabase';
@@ -32,13 +31,10 @@ export const options = {
 };
 
 function countryCode() {
-    for (const key in countryDatabase) {
-        if (refs.selected.textContent === 'Choose country') {
-            return ''
-        } else (key === refs.selected.textContent);{
-            return `${countryDatabase[key]}`
-        }
-    }
+  if (refs.selected.textContent === 'Choose country') {
+    return ''
+  }
+  return countryDatabase[refs.selected.textContent]
 }
 
 
@@ -66,17 +62,19 @@ export function getPagination() {
         if (options.totalItems < 101 && currentPage > 1) {
             const firstButton = document.querySelector('.tui-first')
             firstButton.classList.add('tui-hide')
-        }
+      }
 
-        const discoveryApiService = new DiscoveryApiService();
-        discoveryApiService.page = currentPage - 1
-        discoveryApiService.keyWord = refs.input.value
-        discoveryApiService.countryCode = countryCode()
-        options.page = currentPage
-        const events = await discoveryApiService.getEventsByInputValue()
+      const discoveryApiService = new DiscoveryApiService();
+      discoveryApiService.page = currentPage - 1
 
-        clearEventsList();
-        renderEventsList(events);
+      discoveryApiService.keyWord = refs.input.value
+
+      discoveryApiService.countryCode = countryCode()
+
+      options.page = currentPage
+      const events = await discoveryApiService.getEventsByInputValue()
+      clearEventsList();
+      renderEventsList(events);
 
     })
 }

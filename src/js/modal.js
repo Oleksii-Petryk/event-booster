9aa2f681946a4c644d39b3679bc527706eed4ff5
Mark-Y <yakushyn.mark@gmail.com>
2021-09-18 18:33:04 +0300

    init select_country_bagfix

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,30 +1,32 @@
 import refs from './refs';
 import modalTemplate from '../templates/modal-template.hbs';
 
-    export function renderModal() {
-
-        const modalOpener = modalTemplate();
-        refs.backdrop.insertAdjacentHTML('beforeend', modalOpener);
-    }
-
-    export function onEventCardClick(evt) {
-      evt.preventDefault();
-        if (!evt.target.closest('li').classList.contains('events__item')) {
-          return
-        };
-      refs.backdrop.classList.remove("is-hidden");
-      const closeButton = document.querySelector('.modal__close-button');
-      closeButton.addEventListener('click', closeModal);
-      refs.backdrop.addEventListener('click', onBackDropClick);
+export function renderModal() {
+  const modalOpener = modalTemplate();
+  refs.backdrop.insertAdjacentHTML('beforeend', modalOpener);
 }
-    
+
+export function onEventCardClick(evt) {
+  if (refs.optionsContainer.classList.contains('active')) {
+    return;
+  }
+  evt.preventDefault();
+  if (!evt.target.closest('li').classList.contains('events__item')) {
+    return;
+  }
+  refs.backdrop.classList.remove('is-hidden');
+  const closeButton = document.querySelector('.modal__close-button');
+  closeButton.addEventListener('click', closeModal);
+  refs.backdrop.addEventListener('click', onBackDropClick);
+}
+
 function closeModal(e) {
-  refs.backdrop.classList.add("is-hidden");
+  refs.backdrop.classList.add('is-hidden');
 }
 
 function onBackDropClick(e) {
   if (e.target === e.currentTarget) {
-    refs.backdrop.classList.add("is-hidden");
+    refs.backdrop.classList.add('is-hidden');
     refs.backdrop.removeEventListener('click', onBackDropClick);
   }
 }

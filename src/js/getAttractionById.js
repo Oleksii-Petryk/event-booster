a5de01f61a002961a7095a1390211f092c66a016
Oleksandr Okhrimovich <ohrimovich.sanek@gmail.com>
2021-09-27 22:43:30 +0300

    [getAttractionById] hide pagination

--- src/js/getAttractionById.js
+++ src/js/getAttractionById.js
@@ -13,6 +13,7 @@ export default async function getAttractionById() {
   discoveryApiService.attractionsId = id;
   if (!id) {
     clearEventsList();
+    refs.pagination.style.display = 'none';
     renderMorefromAuthorNotif();
     closeModal();
     return;
